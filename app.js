060850a68f596c4110189c2c30503bb7f8968949
Jeff Pinkston <jeff.pinkston@pearson.com>
2016-03-07 16:16:56 -0600

    Implemented Posts and Upvotes

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
+app.controller('MainController', [
   '$scope', 
   function($scope){
 	$scope.test = 'Hello World!';	
 	$scope.posts = [
-	  'post 1',
-	  'post 2',
-	  'post 3',
-	  'post 4',
-	  'post 5'
+	  {title: 'post 1', upvotes: 5},
+	  {title: 'post 2', upvotes: 2},
+	  {title: 'post 3', upvotes: 15},
+	  {title: 'post 4', upvotes: 9},
+	  {title: 'post 5', upvotes: 4}
 	];
+
+	$scope.addPost = function() {
+		if(!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.title = '';
+	};
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
 }]);
